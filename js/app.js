7e8e085d92dadc5a735d7a47cc83e4ad363dc089
xgoogax <agoogia@gmail.com>
2016-09-17 20:48:09 +0200

    added RWD and changes in design

--- js/app.js
+++ js/app.js
@@ -5,7 +5,39 @@ $(document).ready(function(){
     console.log(achievRight);
     console.log(achievLeft);
     
+    
+    if ($(window).width() < 768){
+    achievLeft.on("click", function(){
+        if ($(".academic").hasClass("hidden")){
+            $(".academic").slideDown(500);
+            $(".academic").toggleClass("hidden");
+            $("#achievements").animate({height: "+=200px"}, 500);
+        }
+        else {
+            $(".academic").slideUp(500);
+            $(".academic").toggleClass("hidden");
+            $("#achievements").animate({height: "-=200px"}, 500);
+
+        }
+    })
+    
+    achievRight.on("click", function(){
+        if ($(".prof").hasClass("hidden")){
+            $(".prof").slideDown(500);
+            $(".prof").toggleClass("hidden");
+            $("#achievements").animate({height: "+=200px"}, 500);
+        }
+        else {
+            $(".prof").slideUp(500);
+            $(".prof").toggleClass("hidden");
+            $("#achievements").animate({height: "-=200px"}, 500);
+        }
+    })
+    }
+    
+    
 //    creating slider for clicking the achievement section
+    else if ($(window).width() >= 768){
     achievLeft.on("click", function(){
         if ($(".academic").hasClass("hidden")){
             $(".academic").slideDown(500);
@@ -32,6 +64,7 @@ $(document).ready(function(){
             $("#achievements").animate({height: "-=150px"}, 500);
         }
     })
+    }
     
 //    creating infinite slider
     var prevButton = $(".prev");
@@ -40,6 +73,7 @@ $(document).ready(function(){
     var ul = $(".slider ul");
     var index = 1;
     var divWidth = 600;
+    var adding = "px";
     
     //cloning
     var cloneFirst = listElem.eq(0).clone();
@@ -51,27 +85,32 @@ $(document).ready(function(){
     //length
     var length = listElem.length;
     
+    if ($(window).width() < 768){
+        divWidth = 90;
+        adding = "vw";
+    }
+        
     ul.css({
-            "width": ((length+2) * divWidth) + "px",
+            "width": ((length+2) * divWidth) + adding,
             "position": "relative",
-            "left": -divWidth+"px" 
+            "left": -divWidth+adding 
     });
     
      nextButton.on("click", function() {
          index++;
         if (index <= length){
-         ul.animate({left: "-=" + divWidth + "px"}, 500, function(){
+         ul.animate({left: "-=" + divWidth + adding}, 500, function(){
              if (index === length){
                  index = 0;
-                 ul.css("left", 0+ "px");
+                 ul.css("left", 0+ adding);
              }
          })
          console.log(index);
         }
            else {
                index = 1;
-               ul.animate({left: -(length+1)*divWidth + "px"}, 500, function(){
-                   ul.css("left", -divWidth + "px");
+               ul.animate({left: -(length+1)*divWidth + adding}, 500, function(){
+                   ul.css("left", -divWidth + adding);
                });
            }
      });
@@ -79,20 +118,21 @@ $(document).ready(function(){
      prevButton.on("click", function() {
          index--;
          if (index >= 0){
-         ul.animate({left: "+=" + divWidth + "px"}, 500, function(){
+         ul.animate({left: "+=" + divWidth + adding}, 500, function(){
              if (index <= 0){
                  index = length;
-                 ul.css("left", -(length)*divWidth + "px");
+                 ul.css("left", -(length)*divWidth + adding);
              }
          })
          console.log(index);
          }
          else {
              index = length -1;
-             ul.animate({left: divWidth + "px"}, 500, function(){
-                 ul.css("left", -(length-1)*divWidth + "px");
+             ul.animate({left: divWidth + adding}, 500, function(){
+                 ul.css("left", -(length-1)*divWidth + adding);
              }) 
          }
   
      });
+    
 });
